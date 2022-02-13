8cceae0b6c1c0a8339bc3d5846d61ba792bd1a85
yo-kayama <kayama0919@gmail.com>
2022-02-13 14:04:31 +0900

    'lgtm/image_source.pyの追加'

---
+++ lgtm/image_source.py
@@ -0,0 +1,11 @@
+import requests
+
+
+class LocalImage:
+    """ファイルから画像を取得する"""
+    
+    def __init__(self, path):
+        self._path = path
+        
+    def get_image(self):
+        return open(self._path, 'rb')
